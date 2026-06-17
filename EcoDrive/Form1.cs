3d16e3e49f1da7edd4ec0b148bdfcc0ce7f16def
Pasindu Gimhana <gimhanapasindu08@gmail.com>
2026-06-17 17:25:32 +0530

    Added professional login UI with placeholders and show password feature

--- EcoDrive/Form1.cs
+++ EcoDrive/Form1.cs
@@ -2,12 +2,13 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Data.SqlClient;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Button;
 
 namespace EcoDrive
 {
@@ -109,6 +110,94 @@ namespace EcoDrive
         private void btnDownloadReceipt_Click_1(object sender, EventArgs e)
         {
            
+        }
+
+        private void btnAdminLogin_Click_1(object sender, EventArgs e)
+        {
+            // 1. chiking empty boxes
+            if (string.IsNullOrEmpty(txtUsername.Text) || string.IsNullOrEmpty(txtPassword.Text))
+            {
+                MessageBox.Show("Please enter both Username and Password!", "Required Fields", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            // 2. Cheking Username Password 
+            if (txtUsername.Text == "admin" && txtPassword.Text == "admin123")
+            {
+                MessageBox.Show("Login Successful! Welcome to Admin Panel.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                // Admin Dashboard Form එක ඕපන් කරනවා
+                AdminDashboardForm dashboard = new AdminDashboardForm();
+                dashboard.Show();
+
+              
+                 this.Hide(); 
+            }
+            else
+            {
+                // 3. Showing Error massage
+                MessageBox.Show("Wrong Username or Password! Please try again.", "Login Failed", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                txtPassword.Clear(); 
+                txtPassword.Focus(); 
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBoxShowPassword.Checked)
+            {
+                txtPassword.UseSystemPasswordChar = false;
+            }
+            else
+            {
+                txtPassword.UseSystemPasswordChar = true;
+            }
+        }
+
+        private void txtUsername_Enter(object sender, EventArgs e)
+        {
+            if (txtUsername.Text == "Enter Username...")
+            {
+                txtUsername.Text = "";
+                txtUsername.ForeColor = Color.Black; 
+            }
+        }
+
+        private void txtUsername_Leave(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtUsername.Text))
+            {
+                txtUsername.Text = "Enter Username...";
+                txtUsername.ForeColor = Color.Gray;
+            }
+        }
+
+        private void txtPassword_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtPassword_Enter(object sender, EventArgs e)
+        {
+            if (txtPassword.Text == "Enter Password...")
+            {
+                txtPassword.Text = "";
+                txtPassword.ForeColor = Color.Black; 
+
+                
+                txtPassword.UseSystemPasswordChar = !checkBoxShowPassword.Checked;
+            }
+        }
+
+        private void txtPassword_Leave(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtPassword.Text))
+            {
+                txtPassword.Text = "Enter Password...";
+                txtPassword.ForeColor = Color.Gray;
+                txtPassword.UseSystemPasswordChar = false; 
+            }
         }
     }
 }
